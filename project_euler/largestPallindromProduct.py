51cc79ccac425ebe73ea91523d80869f51340d1e
arunkgupta <get.arungupta@gmail.com>
2015-12-28 18:55:01 +0530

    completed largest pallindrom problem

--- project_euler/largestPallindromProduct.py
+++ project_euler/largestPallindromProduct.py
@@ -1,3 +1,18 @@
 
 
+def largest_number():
+    l = []
+    for i in range(999,100,-1):
+        for j in range(999,100,-1):
+            if is_pallindrom(i*j):
+                print i, j, i*j
+                l.append((i * j))
+    return max(l)
+
+def is_pallindrom(number):
+    now_string = str(number)
+    return now_string == now_string[::-1]
+
 if __name__ == '__main__':
+     p = largest_number()
+     print p
